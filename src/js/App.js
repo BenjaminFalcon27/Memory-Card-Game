ad2fa7cdf55e3596e4c516f8c8e0ca517891802f
BenjaminFalcon27 <85941055+BenjaminFalcon27@users.noreply.github.com>
2022-06-03 17:06:33 +0200

    police bug on heroku

--- src/js/App.js
+++ src/js/App.js
@@ -6,11 +6,11 @@ function App() {
   return (
     <div className="App">
       <link
-        href="http://fonts.cdnfonts.com/css/new-super-mario-font-u"
+        href="https://fonts.cdnfonts.com/css/new-super-mario-font-u"
         rel="stylesheet"
       ></link>
       <style>
-        @import url('http://fonts.cdnfonts.com/css/new-super-mario-font-u');
+        @import url('https://fonts.cdnfonts.com/css/new-super-mario-font-u');
       </style>
       <header className="App-header">
         <h1 className="game-title">Mario Memory Card Game</h1>
